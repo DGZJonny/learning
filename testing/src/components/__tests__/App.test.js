28be4dca70edc6d2e1aaf33cb8ad8e7b98f92734
unknown <jonathan.deguzman@aceproject.space>
2018-07-28 13:17:45 -0500

    first commit

---
+++ testing/src/components/__tests__/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+//let instead of wrapped since it allows us to redefine variable multiple times
+let wrapped;
+
+//before every single test
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it('shows a comment box', () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
